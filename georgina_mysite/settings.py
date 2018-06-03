8c0dbd0090ab5d2906a0be538b7e9aab6cdb41c4
ismael <ismael@example.com>
2018-06-03 16:54:28 +0200

    Siguiendo tutoril django girls

--- georgina_mysite/settings.py
+++ georgina_mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite',
+    'myprofile',
+    'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
