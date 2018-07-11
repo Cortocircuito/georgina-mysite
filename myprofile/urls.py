7427d5e315f1777efa5979dda1e0cfff53e145ec
cortocircuito <cortocircuit@gmail.com>
2018-07-11 11:56:01 +0200

    Remove contact view

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^contacto', views.contacto),
     url(r'^galeria', views.galeria),
     url(r'^insta', views.insta),
 ]
