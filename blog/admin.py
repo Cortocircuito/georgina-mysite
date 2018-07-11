1f94552e056d36c633fe168a6c4ca959125836e7
cortocircuito <cortocircuit@gmail.com>
2018-07-11 08:17:13 +0200

    Personalización de la interfaz de administración de edición de Posts

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'text',)
+    search_fields = ('title', 'text',)
+    list_filter = ('published_date',)
+    date_hierarchy = 'published_date'
+    ordering = ('-published_date',)
+    fields = ('title', 'author', 'text', 'published_date',)
+
+
+admin.site.register(Post, PostAdmin)
